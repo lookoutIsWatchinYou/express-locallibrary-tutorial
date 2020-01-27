79ad3a7ea1dcc4be5a0e05e22f8ab866182f0189
lookoutIsWatchinYou <58788441+lookoutIsWatchinYou@users.noreply.github.com>
2020-01-27 10:44:44 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var createError = require('http-errors');
 var express = require('express');
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://themat7:Madassif12345@cluster0-6jqdr.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url ='mongodb+srv://themat7:Madassif12345@cluster0-6jqdr.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
